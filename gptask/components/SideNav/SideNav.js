75b4c6b578e6ce84a33219acdfcc7c2a75fc60d0
Ryan Aloof <r-aloof@outlook.com>
2023-02-16 02:36:55 -0800

    added margin for main content area

--- gptask/components/SideNav/SideNav.js
+++ gptask/components/SideNav/SideNav.js
@@ -11,6 +11,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { CalendarPicker } from '@mui/x-date-pickers/CalendarPicker';
 
+// don't change without modifying css "container" class too (in globals.css)
 const drawerWidth = 350;
 
 export default function SideNav() {
