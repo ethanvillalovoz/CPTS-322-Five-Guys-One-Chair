87c9c1901e48916678d33b532c923efc66000013
Ryan Aloof <r-aloof@outlook.com>
2023-02-16 01:32:38 -0800

    added roboto font

--- gptask/pages/_app.js
+++ gptask/pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({
+  weight: ['300','400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
