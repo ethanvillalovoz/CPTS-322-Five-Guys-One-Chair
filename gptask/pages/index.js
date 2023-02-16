83c7bbb85726da6e0b6b0c7741e056fe9045a932
Ryan Aloof <r-aloof@outlook.com>
2023-02-16 02:16:31 -0800

    package modifications and index.js change that didn't go with last commit, oops

--- gptask/pages/index.js
+++ gptask/pages/index.js
@@ -1,20 +1,5 @@
 import Head from 'next/head'
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { CalendarPicker } from '@mui/x-date-pickers/CalendarPicker';
-
-const drawerWidth = 240;
+import SideNav from 'components/SideNav/SideNav';
 
 export default function Home() {
   return (
@@ -25,35 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello, World!</h1>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar />
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <CalendarPicker></CalendarPicker>
-      </Drawer>
+      <SideNav></SideNav>
     </>
   )
 }
