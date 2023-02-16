1aab25644ebf197ed79bdcc873d6363850afe323
Ryan Aloof <r-aloof@outlook.com>
2023-02-16 02:14:08 -0800

    added sidenav component

---
+++ gptask/components/SideNav/SideNav.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { CalendarPicker } from '@mui/x-date-pickers/CalendarPicker';
+
+const drawerWidth = 350;
+
+export default function SideNav() {
+    return (
+        <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar>
+          Hello, World!
+        </Toolbar>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <CalendarPicker onChange={() => alert('hello')}></CalendarPicker>
+        </LocalizationProvider>
+      </Drawer>
+    );
+}
